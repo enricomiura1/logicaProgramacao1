40fc590fc01b2e868a0080b27c2ca48b374a3c0e
Enrico <enrico.epetro@gmail.com>
2020-08-01 13:17:31 -0300

    commit logica programacao

---
+++ LogicaProgramacao1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace LogicaProgramacao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            bool souRico = false;
+            double salario = 200000;
+
+        if (souRico || salario >= 50000)
+            {
+               Console.WriteLine("Voce e rico ");
+            }
+        
+
+   
+
+        //{
+        //    string sobremesa = "Pudim";
+        //    bool estouAcimaDoPeso = false;
+
+        //    if (estouAcimaDoPeso || sobremesa == "Pudim")
+        //    {
+        //        Console.WriteLine("Comer Sobremesa");
+        //    }
+
+            //bool termineiTrabalho = false;
+            //bool estouComFome = true;
+
+            //if (!termineiTrabalho && estouComFome)
+            //{
+            //    Console.WriteLine("Almoçar");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Continuar trabalhando");
+            //}
+            //int numero = 10;
+            //bool boolean = true;
+
+            //if (numero != 20 && boolean == true)
+            ////{
+            ////    Console.WriteLine("A condição é verdadeira");
+            ////}
+            ////else
+            ////{
+            ////    Console.WriteLine("A condição é falsa");
+            //}
+
+            //// Aprendendo sobre Concatenação
+            ////string bemVindo = "Seja bem vindo,";
+            ////string nome = "Enrico";
+            ////string frase = bemVindo + nome;
+            ////Console.WriteLine(frase);
+            //string nome = "Enrico";
+            //string idade = "20";
+            //string bemVindo = $"Seja bem vindo,{nome}. Sua idade é de: {idade} anos";
+            //Console.WriteLine(bemVindo);
+
+
+            //// Expressões logicas e CONSTANTES
+            //const int constInteiro = 20;
+            //int inteiro1 = 10;
+            //int inteiro2 = 20;
+            //int soma = inteiro1 + inteiro2;
+            //Console.WriteLine(soma);
+            //inteiro1 = 50 + soma;
+            //Console.WriteLine(inteiro1 * 2);
+
+
+            // Aprendendo a declarar variáveis
+            //string hello = "Seja Bem Vindo a nossa aula!";
+            //int inteiro = 1;
+            //double numero = 1.5;
+            //bool booleano = false;
+
+
+            // Aprendendo sobre Arrays
+            //int[] numeros = new int[5];
+            //numeros[0] = 10;
+            //numeros[1] = 20;
+            //numeros[2] = 50;
+            //numeros[3] = 100;
+            //numeros[4] = 200;
+
+
+
+
+
+        }
+
+        private static void NewMethod()
+        {
+            Console.WriteLine("ALmoçar");
+        }
+    }
+}
